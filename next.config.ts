83610798996526f0319e85728773f9d0a13f07c3
vihisantos <vitrsv462@gmail.com>
2026-01-23 20:42:16 -0300

    chore: fix github pages deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? '/CapyFlow-Academy' : '',
+  assetPrefix: isProd ? '/CapyFlow-Academy/' : '',
 };
+
 
 export default nextConfig;
